9c79a31abab3f5105d458022f8235be161e7beb6
huhongjun <13114837@qq.com>
2019-09-05 19:53:08 +0800

    脚本修订，符合自己的环境

--- grid-app/proxy/run-manager-proxy.sh
+++ grid-app/proxy/run-manager-proxy.sh
@@ -1,5 +1,4 @@
 echo "--> Updating terminal app.js"
 cp /home/source/terminal-server/app.js /home/run/terminal-server/app.js
-
 echo "--> Run manager proxy, starting manager.go (compiling with go run ...)"
 go run manager.go
